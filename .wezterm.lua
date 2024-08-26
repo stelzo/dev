48aa2572dda73d37e24a9cbab379ce0c1365067d
stelzo <stelzo@steado.de>
2024-08-26 10:09:47 +0200

    use title

--- .wezterm.lua
+++ .wezterm.lua
@@ -35,7 +35,7 @@ config.font_size = 16
 
 config.enable_tab_bar = false
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
 config.initial_rows = 30
